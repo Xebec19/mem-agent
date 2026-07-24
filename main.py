b40a2b1c97fc0084c13b265290bb9fc3fff53190
xebec19 <rohandeveloper106@gmail.com>
2026-07-24 21:04:53 +0530

    wip mem0 for agent

--- main.py
+++ main.py
@@ -1,3 +1,25 @@
+from mem0 import Memory
+
+config = {
+    "version": "v.1.1",
+    "embedder": {
+        "provider": "openai",
+        "config": {
+            "provider": "openai",
+            "api_key": "",
+            "model": "text-embedding-3-small",
+        },
+    },
+    "llm": {"provider": "openai", "config": {"api_key": "", "model": "gpt-4.1"}},
+    "vector_store": {
+        "provider": "qdrant",
+        "config": {"host": "localhost", "port": 6333},
+    },
+}
+
+mem_client = Memory.from_config(config)
+
+
 def main():
     print("Hello from mem-agent!")
 
